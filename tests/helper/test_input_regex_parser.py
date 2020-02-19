4b3e4bebc8b6c60e4bb089545d1c349b92b3a67f
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2020-02-19 11:00:17 +0100

    test: input regex parser

--- tests/helper/test_input_regex_parser.py
+++ tests/helper/test_input_regex_parser.py
@@ -15,10 +15,11 @@ def assert_callable_parse_input():
 
 
 @pytest.mark.parametrize('input, first_group, second_group', [
-    pytest.param('89', '89', None),
-    pytest.param('89', '89', 'ms'),
-    pytest.param('-89', '-89', 'ms'),
-    pytest.param('-89.90', '-89.90', 'ms'),
+    pytest.param('89', '89', None, id='no misure unit'),
+    pytest.param('89 ms', '89', 'ms', id='with misure unit'),
+    pytest.param('-89 ms', '-89', 'ms', id='unit and negative value'),
+    pytest.param('-89.90', '-89.90', 'ms', id='unit and negative dec value'),
+    pytest.param('-89.90  ms', '-89.90', 'ms', id='multiple spaces value'),
 ])
 def test_parse_values(input, first_group, second_group):
     result = parse_input(input=input)
