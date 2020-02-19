800963237cc81d3a9efd0a63568c2e2fa94ac059
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2020-02-19 11:30:28 +0100

    add: recognixed unit misure module

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	@echo 'installing all dependencies (also the dev ones)'
 	poetry install
 
-test: ## run all tests
+test: install ## run all tests
 	poetry run pytest -vv --cov=src --cov-report term-missing
 
 lint: ## lint code
---
+++ tests/helper/test_recognized_unit_measure.py
@@ -0,0 +1,38 @@
+import pytest
+
+from env_time_converter.helper import recognized_unit_measure
+
+
+@pytest.mark.parametrize('unit', [
+    's', 'seconds', 'sec'
+])
+def test_seconds_unit(unit):
+    assert unit in recognized_unit_measure.seconds_allowed_strings()
+
+
+@pytest.mark.parametrize('unit', [
+    'ms', 'milliseconds', 'm'
+])
+def test_milliseconds_unit(unit):
+    assert unit in recognized_unit_measure.milliseconds_allowed_strings()
+
+
+@pytest.mark.parametrize('unit', [
+    'h', 'hours', 'hrs', 'hour'
+])
+def test_hour_unit(unit):
+    assert unit in recognized_unit_measure.hours_allowed_strings()
+
+
+@pytest.mark.parametrize('unit', [
+    'd', 'days', 'day'
+])
+def test_days_unit(unit):
+    assert unit in recognized_unit_measure.days_allowed_strings()
+
+
+@pytest.mark.parametrize('unit', [
+    'y', 'years', 'yrs', 'year'
+])
+def test_years_unit(unit):
+    assert unit in recognized_unit_measure.years_allowed_strings()
